9af8164fff5a72558f4ad350d7a2cb910bd1377e
Oliver Clerc <oliver.clerc@bitforge.ch>
2021-10-25 17:28:21 +0200

    Fix 'hit-test' bug

--- src/components/WebXR.vue
+++ src/components/WebXR.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="webxr">
     <canvas ref="glcanvas" class="glcanvas"></canvas>
-    <div class="slider">
-      <div class="slides">
-        <div v-for="(model, index) in models" :key="index" class="slide">
-          <button
-            class="slide"
-            :style="{ backgroundImage: 'url(' + model.image + ')' }"
-            @click="activateXR()"
-          ></button>
+    <button slot="ar-button" id="ar-button" @click="activateXR()">
+      View in your Space
+    </button>
+    <div claas="domOverlay" ref="domOverlay">
+      <div class="touch" ref="touch" @click="placeModel()"></div>
+      <div class="slider" ref="slider">
+        <div class="slides">
+          <div v-for="(model, index) in models" :key="index" class="slide">
+            <button
+              v-if="model.id == selectedModelId"
+              class="slide selected"
+              :style="{ backgroundImage: 'url(' + model.image + ')' }"
+            ></button>
+            <button
+              v-else
+              class="slide"
+              :style="{ backgroundImage: 'url(' + model.image + ')' }"
+              @click="updateSelectedModelId(model.id)"
+            ></button>
+          </div>
         </div>
       </div>
     </div>
@@ -33,10 +45,27 @@ export default class WebXr extends Vue {
   private apiKey = "Token bf51a81599630627c69dfb90561983627ef1e66f";
   // Genie Demo Projekt
   private projectId = "dfe91633-9d4c-4cf3-be2e-eb0ab036add3";
-  // Models
+  // Models from Project (Genie Demo)
   private models: Model[] = [];
   // selected button/model
-  private selectedModelId = "f555f1c0-c6b9-4c76-97f9-57d103141550";
+  private selectedModelId = "";
+
+  // WebXR:
+  private modelsObjecdt3D: { [id: string]: Group } = {};
+  private gltfLoader = new GLTFLoader();
+  private reticle: Group;
+  private scene = new Scene();
+  // private canvas = this.$refs.glcanvas;
+  // private gl = this.canvas.getContext("webgl", {
+  //   xrCompatible: true,
+  // }) as WebGLRenderingContext;
+  // private renderer = new WebGLRenderer({
+  //   alpha: true,
+  //   preserveDrawingBuffer: true,
+  //   canvas: this.canvas,
+  //   context: this.gl,
+  // });
+  private camera = new PerspectiveCamera();
 
   private async mounted(): Promise<void> {
     try {
@@ -45,6 +74,9 @@ export default class WebXr extends Vue {
       );
       const modelList = await modelApi.modelsList({ project: this.projectId });
       this.updateModels(modelList);
+      // setting first model as selected
+      this.updateSelectedModelId(this.models[0].id);
+      this.loadReticle();
     } catch (error: any) {
       console.log(error);
     }
@@ -58,17 +90,39 @@ export default class WebXr extends Vue {
     this.selectedModelId = modelId;
   }
 
+  private loadReticle(): void {
+    this.gltfLoader.load(
+      "https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf",
+      (gltf) => {
+        this.reticle = gltf.scene;
+        this.reticle.visible = false;
+        this.scene.add(this.reticle);
+      }
+    );
+  }
+
+  private loadModelsObject3D(): void {
+    for (let model of this.models) {
+      this.gltfLoader.load(
+        model.glb,
+        (gltf) => (this.modelsObjecdt3D[model.id] = gltf.scene)
+      );
+    }
+  }
+
+  private placeModel(): void {
+    if (this.modelsObjecdt3D[this.selectedModelId]) {
+      const clone = this.modelsObjecdt3D[this.selectedModelId].clone();
+      clone.position.copy(this.reticle.position);
+      this.scene.add(clone);
+    }
+  }
+
   private async activateXR(): Promise<void> {
-    // Canvas
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const canvas = this.$refs.glcanvas;
     const gl = canvas.getContext("webgl", {
       xrCompatible: true,
     }) as WebGLRenderingContext;
-
-    // Scene
-    const scene = new Scene();
-
     // Lightning
     // const directionalLight = new THREE.DirectionalLightHelper(0xffffff, 0.3);
     // directionalLight.position.set(10, 15, 10);
@@ -78,110 +132,85 @@ export default class WebXr extends Vue {
     const renderer = new WebGLRenderer({
       alpha: true,
       preserveDrawingBuffer: true,
-      canvas: canvas,
+      canvas: this.canvas,
       context: gl,
     });
     renderer.autoClear = false;
 
     // Camera    const camera = new THREE.PerspectiveCamera();
-    const camera = new PerspectiveCamera();
-    camera.matrixAutoUpdate = false;
-
-    var xr = (navigator as any).xr;
+    this.camera.matrixAutoUpdate = false;
 
     // Session
+    var xr = (navigator as any).xr;
     const session = await xr.requestSession("immersive-ar", {
       requiredFeatures: ["hit-test"],
+      optionalFeatures: ["dom-overlay"],
+      domOverlay: { root: this.$refs.domOverlay },
     });
     session.updateRenderState({
       baseLayer: new XRWebGLLayer(session, gl),
     });
-
     // A 'local' reference space has a native origin that is located
     // near the viewer's position at the time the session was created.
     const referenceSpace = await session.requestReferenceSpace("local");
-
     // Create another XRReferenceSpace that has the viewer as the origin.
     const viewerSpace = await session.requestReferenceSpace("viewer");
     // Perform hit testing using the viewer as origin.
     const hitTestSource = await session.requestHitTestSource({
       space: viewerSpace,
     });
-
     // GLTFLoader
-    const gltfLoader = new GLTFLoader();
-    let reticle: Group;
-    gltfLoader.load(
-      "https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf",
-      function (gltf) {
-        reticle = gltf.scene;
-        reticle.visible = false;
-        scene.add(reticle);
-      }
-    );
 
-    var models3dObject: { [id: string]: Group } = {};
-    for (let model of this.models) {
-      gltfLoader.load(
-        model.glb,
-        (gltf) => (models3dObject[model.id] = gltf.scene),
-        () => null,
-        (error) => console.error("Model Error:", model.id)
-      );
-    }
+    this.loadModelsObject3D();
 
-    console.log(models3dObject);
+    // session.addEventListener("seelct", (evt: Event) => {
+    //   this.placeModel();
+    // });
 
-    session.addEventListener("select", (evt: Event) => {
-      if (models3dObject[this.selectedModelId]) {
-        const clone = models3dObject[this.selectedModelId].clone();
-        clone.position.copy(reticle.position);
-        scene.add(clone);
-      }
-    });
+    // session.addEventListener("select", (evt: Event) => {
+    //   if (this.modelsObjecdt3D[this.selectedModelId]) {
+    //     const clone = this.modelsObjecdt3D[this.selectedModelId].clone();
+    //     clone.position.copy(this.reticle.position);
+    //     this.scene.add(clone);
+    //   }
+    // });
 
     // Create a render loop that allows us to draw on the AR view.
     const onXRFrame = (time: number, frame: XRFrame) => {
       // Queue up the next draw request.
       session.requestAnimationFrame(onXRFrame);
-
       // Bind the graphics framebuffer to the baseLayer's framebuffer
       gl.bindFramebuffer(
         gl.FRAMEBUFFER,
         session.renderState.baseLayer.framebuffer
       );
-
       // Retrieve the pose of the device.
       // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
       const pose = frame.getViewerPose(referenceSpace);
       if (pose) {
         // In mobile AR, we only have one view.
         const view = pose.views[0];
-
         const viewport = session.renderState.baseLayer.getViewport(view);
         renderer.setSize(viewport.width, viewport.height);
-
         // Use the view's transform matrix and projection matrix to configure the THREE.camera.
-        camera.matrix.fromArray(view.transform.matrix);
-        camera.projectionMatrix.fromArray(view.projectionMatrix);
-        camera.updateMatrixWorld(true);
-
+        this.camera.matrix.fromArray(view.transform.matrix);
+        this.camera.projectionMatrix.fromArray(view.projectionMatrix);
+        this.camera.updateMatrixWorld(true);
         const hitTestResults = frame.getHitTestResults(hitTestSource);
-        if (hitTestResults.length > 0 && reticle) {
+        if (hitTestResults.length > 0 && this.reticle) {
           const hitPose = hitTestResults[0].getPose(referenceSpace);
           if (hitPose) {
-            reticle.visible = true;
-            reticle.position.set(
+            this.reticle.visible = true;
+            this.reticle.position.set(
               hitPose.transform.position.x,
               hitPose.transform.position.y,
               hitPose.transform.position.z
             );
           }
-          reticle.updateMatrixWorld(true);
+          this.reticle.updateMatrixWorld(true);
         }
-
         // Render the scene with THREE.WebGLRenderer.
-        renderer.render(scene, camera);
+        renderer.render(this.scene, this.camera);
       }
     };
     session.requestAnimationFrame(onXRFrame);
@@ -190,20 +219,77 @@ export default class WebXr extends Vue {
 </script>
 
 <style scoped>
+.webxr {
+  overflow-x: hidden;
+}
+
 .glcanvas {
   z-index: 1;
+  transform: translateZ(0);
+  pointer-events: none;
+}
+
+#ar-button {
+  background-image: url("https://modelviewer.dev/assets/ic_view_in_ar_new_googblue_48dp.png");
+  background-repeat: no-repeat;
+  background-size: 20px 20px;
+  background-position: 12px 50%;
+  background-color: #fff;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  white-space: nowrap;
+  bottom: 132px;
+  padding: 0px 16px 0px 40px;
+  font-family: Roboto Regular, Helvetica Neue, sans-serif;
+  font-size: 14px;
+  color: #4285f4;
+  height: 36px;
+  line-height: 36px;
+  border-radius: 18px;
+  border: 1px solid #dadce0;
+  z-index: 2;
+}
+
+#ar-button:active {
+  background-color: #e8eaed;
+}
+
+#ar-button:focus {
+  outline: none;
+}
+
+#ar-button:focus-visible {
+  outline: 1px solid #4285f4;
+}
+
+.touch {
+  border: 5px solid orange;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 121px;
+  position: absolute;
 }
 
 .slider {
-  z-index: 2;
   width: 100%;
   text-align: center;
   overflow: hidden;
   position: absolute;
-  bottom: 16px;
+  border: 2px solid red;
+  z-index: 2;
+  left: 0;
+  right: 0;
+  height: 120px;
+  bottom: 0;
 }
 
 .slides {
+  position: absolute;
+  bottom: 16px;
+  left: 0;
+  right: 0;
   display: flex;
   overflow-x: auto;
   scroll-snap-type: x mandatory;
@@ -224,11 +310,18 @@ export default class WebXr extends Vue {
   border-radius: 10px;
   border: none;
   display: flex;
-  border-inline-start-color: #4285f4;
-  border: solid 1px #4285f4;
+  /* border: 1px solid #4285f4; */
 }
 
 .slide.selected {
   border: 2px solid #4285f4;
+}
+
+.slide:focus {
+  outline: none;
+}
+
+.slide:focus-visible {
+  outline: 1px solid #4285f4;
 }
 </style>
