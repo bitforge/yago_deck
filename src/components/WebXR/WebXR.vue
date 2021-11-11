a13b3f4a05051b8657d63792e4f59d194d640d66
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2021-11-11 13:27:18 +0100

    Improve tool buttons UX/UI

--- src/components/WebXR/WebXR.vue
+++ src/components/WebXR/WebXR.vue
@@ -4,15 +4,21 @@
 
         <div class="launcher">
             <button slot="ar-button" id="ar-button" @click="activateXR">
-                <img src="~@/assets/img/ar-icon.svg" />
+                <img src="~@/assets/img/ar_icon.svg" />
                 Launch Genie WebXR Demo
             </button>
         </div>
 
         <div class="domOverlay" ref="domOverlay" v-show="xrSessionActive">
-            <div class="removeButtons">
-                <button class="removeAllButton" @click="removeAllModels">Remove all objects</button>
-                <button class="removeLastButton" @click="removeLastModel">Remove last objects</button>
+            <div class="toolbar">
+                <button @click="removeLastModel">
+                    <img src="~@/assets/img/undo.svg" />
+                    <span>Undo</span>
+                </button>
+                <button @click="removeAllModels">
+                    <img src="~@/assets/img/delete.svg" />
+                    <span>Clear</span>
+                </button>
             </div>
             <div class="touch" ref="touch" @click="placeModel">
                 <p>Tap anywhere to place!</p>
