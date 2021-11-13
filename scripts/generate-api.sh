446f86082823e3df5644ee0923aa4a6a49d39afa
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2021-11-13 13:17:06 +0100

    Use npx to run openapi generator

--- scripts/generate-api.sh
+++ scripts/generate-api.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
 cd "$(dirname $0)/.."
 
-# Install openapi-generator:
-# npm install -g @openapitools/openapi-generator-cli
-
-exec openapi-generator-cli generate \
+# Fetch OpenAPI Schema an generate api client
+exec npx @openapitools/openapi-generator-cli generate \
     -i http://genie-ar.ch/api/schema/ \
     -o src/api/ \
     -g typescript-fetch \
