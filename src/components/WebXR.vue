2017f7568cfa8c9640a7b23acea1f490aac2cbe1
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2021-11-13 01:05:45 +0100

    Hide preview model while new model is still loading

--- src/components/WebXR.vue
+++ src/components/WebXR.vue
@@ -43,6 +43,7 @@ export default class WebXr extends Vue {
     private camera = new THREE.PerspectiveCamera();
     private directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     private lightProbe = new THREE.LightProbe();
+    private previewMaterial: THREE.MeshStandardMaterial | null = null;
 
     public mounted(): void {
         // Prepare THREE.js Scene
@@ -248,34 +249,45 @@ export default class WebXr extends Vue {
     private updatePreviewModel(): void {
         // Nopsy is absolutely required!
         if (!this.nopsy || !this.selectedModel) return;
+        const isSelectedModel = this.previewModel?.name == `model-${this.selectedModel?.id}`;
 
-        // Update preview model only when necessary
-        const previewModelUpdateNeeded =
-            this.models3D[this.selectedModel.id] && this.previewModel?.name != `model-${this.selectedModel.id}`;
+        // Remove preview model when 3D Model is loaded yet
+        const removePreviewModel = !isSelectedModel && !this.models3D[this.selectedModel?.id];
+        if (removePreviewModel && this.previewModel) {
+            this.nopsy.remove(this.previewModel);
+            this.previewModel = null;
+        }
 
-        if (previewModelUpdateNeeded) {
-            // Remove old version when present
-            if (this.previewModel) this.nopsy.remove(this.previewModel);
+        // Add preview model when loaded
+        const addPreviewModel = !isSelectedModel && this.models3D[this.selectedModel?.id];
+        if (addPreviewModel) {
             // Add new semi transparent clone
             this.previewModel = this.models3D[this.selectedModel.id].clone();
             this.previewModel.traverse((object: THREE.Object3D<THREE.Event>) => {
                 const mesh = object as THREE.Mesh;
                 const material = mesh.material as THREE.MeshStandardMaterial;
-                if (material) {
-                    const transMat = new THREE.MeshStandardMaterial({
-                        color: 0xffffff,
-                        roughness: 0.5,
-                        metalness: 0.5,
-                    });
-                    transMat.format = THREE.RGBAFormat;
-                    transMat.transparent = true;
-                    transMat.opacity = 0.3;
-                    transMat.side = THREE.DoubleSide;
-                    mesh.material = transMat;
-                }
+                if (material) mesh.material = this.getPreviewMaterial();
             });
             this.nopsy.add(this.previewModel);
         }
+    }
+
+    private getPreviewMaterial(): THREE.MeshStandardMaterial {
+        // Only create onces
+        if (this.previewMaterial) return this.previewMaterial;
+
+        // Create and keep preview material
+        const transMat = new THREE.MeshStandardMaterial({
+            color: 0xffffff,
+            roughness: 0.5,
+            metalness: 0.5,
+        });
+        transMat.format = THREE.RGBAFormat;
+        transMat.transparent = true;
+        transMat.opacity = 0.3;
+        transMat.side = THREE.DoubleSide;
+        this.previewMaterial = transMat;
+        return this.previewMaterial;
     }
 
     private updateLightningEstimate(frame: XRFrame): void {
