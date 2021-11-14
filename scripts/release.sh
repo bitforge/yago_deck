578088542adc9a309a4fea2b7df72025856dd58c
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2021-11-14 18:56:01 +0100

    Set release target to genie-deck

--- scripts/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-cd "$(dirname $0)/.."
-PUR='\033[0;32m'
-EOC='\033[0m'
-printf "${PUR}BUILD${EOC}\n"
-yarn build --non-interactive --noprogress
-printf "${PUR}DEPLOY${EOC}\n"
-exec yarn deploy
